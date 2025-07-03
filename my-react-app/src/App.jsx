7074318fa9984f27536b80da19a9b6675216ab43
germansz10 <sanchezgermanalejandro@gmail.com>
2025-07-03 19:24:53 -0300

    modificacion navbar

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar/NavBar';
+
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import NavBar from './components/NavBar/NavBar';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import { CartProvider } from './context/CartContext';
 import Cart from './components/Cart/Cart';
