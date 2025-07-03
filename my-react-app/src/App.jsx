899fcf5ee112b8530d865f39dbe3b99286a9337d
germansz10 <sanchezgermanalejandro@gmail.com>
2025-07-03 19:58:58 -0300

    Subiendo cambios

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import NavBar from './components/NavBar/NavBar';
+import NavBar from './components/Navbar/NavBar';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import { CartProvider } from './context/CartContext';
 import Cart from './components/Cart/Cart';
